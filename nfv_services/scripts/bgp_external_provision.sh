9c84c8cbdd9bdd0607ea6719d9bbdd2ebf2b33cd
Guilherme Aguiar <guilherme.it97@gmail.com>
2025-04-19 16:19:38 -0300

    deploy BGP funcionando

--- nfv_services/scripts/bgp_external_provision.sh
+++ nfv_services/scripts/bgp_external_provision.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 
-# Instalar dependências e FRRouting
+# Instala FRRouting
 sudo apt update
 sudo apt install -y frr frr-pythontools
 
-# Habilitar os daemons necessários
-sudo sed -i 's/bgpd=no/bgpd=yes/' /etc/frr/daemons
-sudo sed -i 's/zebra=no/zebra=yes/' /etc/frr/daemons
+# Ativa serviços do FRR
+sudo sed -i 's/^bgpd=no/bgpd=yes/' /etc/frr/daemons
 sudo systemctl restart frr
 
-# Configurar IP adicional na interface interna
-sudo ip addr add 20.0.0.1/24 dev enp0s8
-sudo ip link set enp0s8 up
+# Configura IPs (evita erro se já estiverem atribuídos)
+sudo ip addr show dev eth1 | grep -q 192.168.33.11 || sudo ip addr add 192.168.33.11/24 dev eth1
+sudo ip addr show dev eth1 | grep -q 20.0.0.1 || sudo ip addr add 20.0.0.1/24 dev eth1
+sudo ip link set dev eth1 up
 
-# Criar configuração do FRR
-cat <<EOF | sudo tee /etc/frr/frr.conf > /dev/null
+# Configuração FRR (vm2)
+sudo tee /etc/frr/frr.conf > /dev/null <<EOF
+frr version 10.3
 frr defaults traditional
 hostname bgp-external
 log file /var/log/frr/frr.log
+no ip forwarding
+no ipv6 forwarding
 service integrated-vtysh-config
-!
-interface enp0s8
+
+interface eth1
  ip address 192.168.33.11/24
  ip address 20.0.0.1/24
-!
+
 router bgp 65002
  bgp router-id 2.2.2.2
  neighbor 192.168.33.10 remote-as 65001
- network 20.0.0.0/24
+
+ address-family ipv4 unicast
+  network 20.0.0.0/24
+  neighbor 192.168.33.10 route-map EXPORT out
+ exit-address-family
+
+route-map EXPORT permit 10
+ set src 20.0.0.1
 !
-line vty
 EOF
 
-# Reiniciar FRR
+# Reinicia serviço
 sudo systemctl restart frr
--- nfv_services/scripts/bgp_internal_provision.sh
+++ nfv_services/scripts/bgp_internal_provision.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
-# Instalar dependências e FRRouting
+# Instala FRRouting
 sudo apt update
 sudo apt install -y frr frr-pythontools
 
-# Habilitar os daemons necessários
-sudo sed -i 's/bgpd=no/bgpd=yes/' /etc/frr/daemons
-sudo sed -i 's/zebra=no/zebra=yes/' /etc/frr/daemons
+# Ativa serviços do FRR
+sudo sed -i 's/^bgpd=no/bgpd=yes/' /etc/frr/daemons
 sudo systemctl restart frr
 
-# Configurar IP adicional na interface interna
-sudo ip addr add 10.0.0.1/24 dev enp0s8
-sudo ip link set enp0s8 up
+# Configura IPs (evita erro se já estiverem atribuídos)
+sudo ip addr show dev eth1 | grep -q 192.168.33.10 || sudo ip addr add 192.168.33.10/24 dev eth1
+sudo ip link set dev eth1 up
 
-# Criar configuração do FRR
-cat <<EOF | sudo tee /etc/frr/frr.conf > /dev/null
+# Configuração FRR (vm1)
+sudo tee /etc/frr/frr.conf > /dev/null <<EOF
+frr version 10.3
 frr defaults traditional
 hostname bgp-internal
 log file /var/log/frr/frr.log
+no ip forwarding
+no ipv6 forwarding
 service integrated-vtysh-config
-!
-interface enp0s8
+
+interface eth1
  ip address 192.168.33.10/24
- ip address 10.0.0.1/24
-!
+
 router bgp 65001
  bgp router-id 1.1.1.1
  neighbor 192.168.33.11 remote-as 65002
- network 10.0.0.0/24
+
+ address-family ipv4 unicast
+  neighbor 192.168.33.11 route-map IMPORT in
+  network 10.0.0.0/24
+ exit-address-family
+
+route-map IMPORT permit 10
 !
-line vty
 EOF
 
-# Reiniciar FRR
+# Reinicia serviço
 sudo systemctl restart frr
